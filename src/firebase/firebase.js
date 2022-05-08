3ad88c95f6094f347665e7a1264b9688d932309b
SI-Encoding <stanscoding12+encoding@gmail.com>
2022-05-08 12:02:32 -0700

    Initial commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/compat'
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+{/* For Firebase JS SDK v7.20.0 and later, measurementId is optional */}
 const firebaseConfig = {
     apiKey: `${process.env.REACT_APP_apiKey}`,
     authDomain: `${process.env.REACT_APP_authDomain}`,
