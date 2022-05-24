7725266d38e0b6d18d4265ca16578b1736190b20
SI-Encoding <stanscoding12+encoding@gmail.com>
2022-05-24 16:29:55 -0700

    Refactored user display mode when logging in.

--- src/App.js
+++ src/App.js
@@ -14,73 +14,15 @@ import Favourites from './components/body/favourites/Favourites'
 import MyPosts from './components/body/myposts/MyPosts';
 import GifPosts from './components/body/gifposts/GifPosts';
 import OtherUsersPosts from './components/body/otherusersposts/OtherUsersPosts';
+import DarkMode from './functions/DarkMode' 
+import LightMode from './functions/LightMode'
 
 function App() {
   const user = useSelector((state) => state.user)
   const darkMode = useSelector((state) => state.darkMode)
 
-  const darkModeColours = {
-    background: '#18191a',
-    storyReel: '#a29b9f',
-    arrow: '#a29b9f',
-    postText: '#a29b9f',
-    messageBorderTop: '#333236',
-    postBorderTop: '#333236',
-    postOption: '#a29b9f',
-    text: '#a29b9f',
-    textField: '#444148',
-    posts: '#2a292e',
-    header: '#2a292e',
-    chatToolbar: '#393b40'
-}
-
-const lightModeColours = {
-    background: '#f1f2f5',
-    storyReel: '#2e81f4',
-    arrow: 'rgba(0, 0, 0, 0.54)',
-    postText: '#000000',
-    messageBorderTop: '#eff2f5',
-    postBorderTop: 'lightgray',
-    postOption: 'gray',
-    text: '#000000',
-    textField: '#eff2f5',
-    posts: 'white',
-    header: 'white',
-    chatToolbar: 'royalblue'
-}
-
-const setPropertyOfDarkMode = () => {
-    document.documentElement.style.setProperty("--fb-theme-colour-white-header", darkModeColours.header);
-    document.documentElement.style.setProperty("--fb-theme-colour-white-posts", darkModeColours.posts);
-    document.documentElement.style.setProperty("--fb-theme-colour-textfield", darkModeColours.textField);
-    document.documentElement.style.setProperty("--fb-theme-colour-text", darkModeColours.text);
-    document.documentElement.style.setProperty("--fb-theme-colour-post-option", darkModeColours.postOption);
-    document.documentElement.style.setProperty("--fb-theme-colour-post-border-top", darkModeColours.postBorderTop);
-    document.documentElement.style.setProperty("--fb-theme-colour-messenger-sender-border-top", darkModeColours.messageBorderTop);
-    document.documentElement.style.setProperty("--fb-theme-colour-post-text", darkModeColours.postText);
-    document.documentElement.style.setProperty("--fb-theme-colour-arrow", darkModeColours.arrow);
-    document.documentElement.style.setProperty("--fb-theme-colour-storyreel", darkModeColours.storyReel);
-    document.documentElement.style.setProperty("--fb-theme-colour-background", darkModeColours.background);
-    document.documentElement.style.setProperty("--fb-theme-colour-chat-toolbar", darkModeColours.chatToolbar);
-}
-
-const setPropertyOfLightMode = () => {
-    document.documentElement.style.setProperty("--fb-theme-colour-white-header", lightModeColours.header);
-    document.documentElement.style.setProperty("--fb-theme-colour-white-posts", lightModeColours.posts);
-    document.documentElement.style.setProperty("--fb-theme-colour-textfield", lightModeColours.textField);
-    document.documentElement.style.setProperty("--fb-theme-colour-text", lightModeColours.text);
-    document.documentElement.style.setProperty("--fb-theme-colour-post-option", lightModeColours.postOption);
-    document.documentElement.style.setProperty("--fb-theme-colour-post-border-top", lightModeColours.postBorderTop);
-    document.documentElement.style.setProperty("--fb-theme-colour-messenger-sender-border-top", lightModeColours.messageBorderTop);
-    document.documentElement.style.setProperty("--fb-theme-colour-post-text", lightModeColours.postText);
-    document.documentElement.style.setProperty("--fb-theme-colour-arrow", lightModeColours.arrow);
-    document.documentElement.style.setProperty("--fb-theme-colour-storyreel", lightModeColours.storyReel);
-    document.documentElement.style.setProperty("--fb-theme-colour-background", lightModeColours.background);
-    document.documentElement.style.setProperty("--fb-theme-colour-chat-toolbar", lightModeColours.chatToolbar);
-}
-
 useEffect(()=> {
-  darkMode?  setPropertyOfDarkMode() :  setPropertyOfLightMode()
+  darkMode?  DarkMode() :  LightMode()
 },[darkMode])
 
 return (
