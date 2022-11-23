98206632d88e9bd0a3eefa0b72eb8313b6b46efe
SI-Encoding <stanscoding12+encoding@gmail.com>
2022-11-23 15:01:43 -0800

    Finished adding unit tests for testing firestore collection.

--- src/test/functions.test.js
+++ src/test/functions.test.js
@@ -21,22 +21,31 @@ const db = firebase.firestore();
 
 describe('test post functionality', () => {
   it('it should add a document to the post collection', async () => {
-    await db.collection('post').doc('1').set({
+    const time = firebaseAdmin.firestore.              
+    FieldValue.serverTimestamp()
+    const postMock = {
       message: '1',              
-      timestamp: firebaseAdmin.firestore.              
-      FieldValue.serverTimestamp(),              
+      timestamp: time,              
+      profilePic: "userPicture",              
+      username: "userName",              
+      favourite: false,
+      gif: false,
+      userId: "userId"
+    }
+    await db.collection('posts').doc('1').set({
+      message: '1',              
+      timestamp: time,              
       profilePic: "userPicture",              
       username: "userName",              
       favourite: false,
       gif: false,
       userId: "userId"
     })
-    const post = await db.collection('post').doc('1').get()
-    
-    expect(post.data()['message']).toEqual("1")
+    const post = await db.collection('posts').doc('1').get()
+    expect(post.data()).toMatchObject(postMock)
   })
-  it('it should update nested document to the post collection', async() => {
-    await db.collection('post').doc('1').set({
+  it('it should update document to the post collection', async() => {
+    await db.collection('posts').doc('1').set({
       message: '2',              
       timestamp: firebaseAdmin.firestore.              
       FieldValue.serverTimestamp(),              
@@ -46,19 +55,49 @@ describe('test post functionality', () => {
       gif: false,
       userId: "userId"
     })
-    const post = await db.collection('post').doc('1').get()
+    const post = await db.collection('posts').doc('1').get()
     
     expect(post.data()['message']).toEqual("2")
   })
 
   it('it should delete the document from the post collection', async() => {
-    await db.collection('post').doc('1').delete()
-    const post = await db.collection('post').doc('1').get()
+    await db.collection('posts').doc('1').delete()
+    const post = await db.collection('posts').doc('1').get()
     expect(post.data()).toEqual(undefined)
   })
 })
 
-describe('test chat functionality', async() => {
+describe('test posts comments functionality', ()=> {
+  it('it should add a nested document to the posts comment collection', async()=> {
+    const time = firebaseAdmin.firestore.FieldValue.serverTimestamp()
+    const postMock = {
+      message: 'comment',
+      time: time,
+      user: 'usernameName',
+      userImage: 'usernamePicture'
+    }
+    await db.collection('posts').doc('1').collection('comments').doc('1').set({
+      message: 'comment',
+      time: time,
+      user: 'usernameName',
+      userImage: 'usernamePicture'
+    })
+    const post = await db.collection('posts').doc('1').collection('comments').doc('1').get()
+    expect(post.data()).toMatchObject(postMock)
+  })
+  it('it should update nested document to the posts comment collection', async()=> {
+  await db.collection('posts').doc('1').collection('comments').doc('1').set({
+    message: '2',
+    time: firebaseAdmin.firestore.FieldValue.serverTimestamp(),
+    user: 'usernameName',
+    userImage: 'usernamePicture'
+  })
+  const post = await db.collection('posts').doc('1').collection('comments').doc('1').get()
+  expect(post.data()['message']).toEqual('2')
+  })
+})
+
+describe('test chat functionality', () => {
   it('it should add a nested document to the chat collection', async() => {
     await db.collection('chat').doc('1').collection('messages').doc('1').collection('message').doc('1').set({
       message: '1',
@@ -101,5 +140,46 @@ describe('test chat functionality', async() => {
     await db.collection('chat').doc('1').delete()
     const chat = await db.collection('chat').doc('1').get()
     expect(chat.data()).toEqual(undefined)
+  })
+})
+
+describe('test friend functionality', () => {
+  it('it should add a nested document to the friend collection' , async() => {
+    await db.collection('users').doc('1').set({
+      friends: {['1']: 'Accept Request'}
+  })
+  db.collection('users').doc('2').set({
+      friends: {['2']: 'Friend Request Sent'}
+  })
+  const friend1 = await db.collection('users').doc('1').get()
+  const friend2 = await db.collection('users').doc('2').get()
+  expect(friend1.data()).toEqual({"friends": {"1": "Accept Request"}})
+  expect(friend2.data()).toEqual({"friends": {"2": "Friend Request Sent"}})
+  })
+
+  it('it should update document to the friend collection' , async() => {
+    await db.collection('users').doc('1').set({
+      friends: {['1']: 'Remove'}
+  })
+  db.collection('users').doc('2').set({
+      friends: {['2']: 'Remove'}
+  })
+  const friend1 = await db.collection('users').doc('1').get()
+  const friend2 = await db.collection('users').doc('2').get()
+  expect(friend1.data()).toEqual({"friends": {"1": 'Remove'}})
+  expect(friend2.data()).toEqual({"friends": {"2": 'Remove'}})
+  })
+
+  it('it should remove document from the friend collection' , async() => {
+    await db.collection('users').doc('1').set({
+      friends: {['1']: 'Add Friend'}
+  })
+  db.collection('users').doc('2').set({
+      friends: {['2']: 'Add Friend'}
+  })
+  const friend1 = await db.collection('users').doc('1').get()
+  const friend2 = await db.collection('users').doc('2').get()
+  expect(friend1.data()).toEqual({"friends": {"1": 'Add Friend'}})
+  expect(friend2.data()).toEqual({"friends": {"2": 'Add Friend'}})
   })
 })
