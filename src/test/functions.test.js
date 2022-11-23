7f3ad46af9a581ff1d3a0ddf6f01dba75b05002d
SI-Encoding <stanscoding12+encoding@gmail.com>
2022-11-23 13:38:43 -0800

    Added unit test for testing firestore chat collection.

--- src/test/functions.test.js
+++ src/test/functions.test.js
@@ -20,7 +20,7 @@ const firebase = firebaseAdmin.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 describe('test post functionality', () => {
-  it('it should add a nested document to the post collection', async () => {
+  it('it should add a document to the post collection', async () => {
     await db.collection('post').doc('1').set({
       message: '1',              
       timestamp: firebaseAdmin.firestore.              
@@ -49,5 +49,57 @@ describe('test post functionality', () => {
     const post = await db.collection('post').doc('1').get()
     
     expect(post.data()['message']).toEqual("2")
+  })
+
+  it('it should delete the document from the post collection', async() => {
+    await db.collection('post').doc('1').delete()
+    const post = await db.collection('post').doc('1').get()
+    expect(post.data()).toEqual(undefined)
+  })
+})
+
+describe('test chat functionality', async() => {
+  it('it should add a nested document to the chat collection', async() => {
+    await db.collection('chat').doc('1').collection('messages').doc('1').collection('message').doc('1').set({
+      message: '1',
+      username: 'username',
+      time: firebaseAdmin.firestore.FieldValue.serverTimestamp(),
+      userId: '1'
+  }).then(() => {
+      db.collection('chat').doc('2').collection('messages').doc('2').collection('message').doc('2').set({
+          message: '1',
+          username: 'username',
+          time: firebaseAdmin.firestore.FieldValue.serverTimestamp(),
+          userId: '2'
+      })
+  })
+  const chat = await db.collection('chat').doc('1').collection('messages').doc('1').collection('message').doc('1').get()
+  const chat2 = await db.collection('chat').doc('2').collection('messages').doc('2').collection('message').doc('2').get()
+  expect(chat.data()['message']).toEqual(chat2.data()['message'])
+  })
+
+  it('it should update nested document to the chat collection', async() => {
+    await db.collection('chat').doc('1').collection('messages').doc('1').collection('message').doc('1').set({
+      message: '2',
+      username: 'username',
+      time: firebaseAdmin.firestore.FieldValue.serverTimestamp(),
+      userId: '1'
+  }).then(() => {
+      db.collection('chat').doc('2').collection('messages').doc('2').collection('message').doc('2').set({
+          message: '2',
+          username: 'username',
+          time: firebaseAdmin.firestore.FieldValue.serverTimestamp(),
+          userId: '2'
+      })
+  })
+  const chat = await db.collection('chat').doc('1').collection('messages').doc('1').collection('message').doc('1').get()
+  const chat2 = await db.collection('chat').doc('2').collection('messages').doc('2').collection('message').doc('2').get()
+  expect(chat.data()['message']).toEqual(chat2.data()['message'])
+  })
+
+  it('it should delete the document from the chat collection', async() => {
+    await db.collection('chat').doc('1').delete()
+    const chat = await db.collection('chat').doc('1').get()
+    expect(chat.data()).toEqual(undefined)
   })
 })
