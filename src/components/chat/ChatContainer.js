b73897f9559011192696f4c3809ed05ae8b61cad
SI-Encoding <stanscoding12+encoding@gmail.com>
2023-01-14 10:22:57 -0800

    Restricted chat to support users that are only logged in.

--- src/components/chat/ChatContainer.js
+++ src/components/chat/ChatContainer.js
@@ -9,18 +9,22 @@ import ChatHeaderInfo from './chatheader/ChatHeaderInfo'
 function ChatContainer() {
   const [expand, setExpand] = useState(false);
   const chatUserInfo = useSelector(state => state.chatUserInfo)
-
+  const user = useSelector(state => state.user);
   const expandMenu = () => {
     setExpand(!expand)
   }
 
   return (
+  <>
+   {user && 
     <div className='chat_toolbar_container'>
-      <ChatHeader expand={expand} expandMenu={expandMenu} chatUserInfo={chatUserInfo}/>
-      {expand && chatUserInfo !== null && <ChatHeaderInfo chatUserInfo={chatUserInfo}/>}
-      {expand && chatUserInfo !== null && <ChatBody chatUserInfo={chatUserInfo}/>}
-      {expand && chatUserInfo === null && <ChatList/>}
+        <ChatHeader expand={expand} expandMenu={expandMenu} chatUserInfo={chatUserInfo}/>
+        {expand && chatUserInfo !== null && <ChatHeaderInfo chatUserInfo={chatUserInfo}/>}
+        {expand && chatUserInfo !== null && <ChatBody chatUserInfo={chatUserInfo}/>}
+        {expand && chatUserInfo === null && <ChatList/>}
     </div>
+  } 
+  </>
   )
 }
 
