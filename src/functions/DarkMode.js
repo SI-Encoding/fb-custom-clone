13de4175941a8b6e3700e1f3a77b1c30c968a8c2
SI-Encoding <stanscoding12+encoding@gmail.com>
2023-01-15 08:21:19 -0800

    Refactored darkmode and lightmode functions.

--- src/functions/DarkMode.js
+++ src/functions/DarkMode.js
@@ -1,31 +1,20 @@
 const setPropertyOfDarkMode = () => {
-    document.documentElement.style.setProperty("--fb-theme-colour-white-header", darkModeColours.header);
-    document.documentElement.style.setProperty("--fb-theme-colour-white-posts", darkModeColours.posts);
-    document.documentElement.style.setProperty("--fb-theme-colour-textfield", darkModeColours.textField);
-    document.documentElement.style.setProperty("--fb-theme-colour-text", darkModeColours.text);
-    document.documentElement.style.setProperty("--fb-theme-colour-post-option", darkModeColours.postOption);
-    document.documentElement.style.setProperty("--fb-theme-colour-post-border-top", darkModeColours.postBorderTop);
-    document.documentElement.style.setProperty("--fb-theme-colour-messenger-sender-border-top", darkModeColours.messageBorderTop);
-    document.documentElement.style.setProperty("--fb-theme-colour-post-text", darkModeColours.postText);
-    document.documentElement.style.setProperty("--fb-theme-colour-arrow", darkModeColours.arrow);
-    document.documentElement.style.setProperty("--fb-theme-colour-storyreel", darkModeColours.storyReel);
-    document.documentElement.style.setProperty("--fb-theme-colour-background", darkModeColours.background);
-    document.documentElement.style.setProperty("--fb-theme-colour-chat-toolbar", darkModeColours.chatToolbar);
+    darkModeColours.map((darkColours) => document.documentElement.style.setProperty(darkColours.cssVariable, darkColours.hexColourCode));
 }
-    
-const darkModeColours = {
-    background : '#18191a',
-    storyReel : '#a29b9f',
-    arrow : '#a29b9f',
-    postText : '#a29b9f',
-    messageBorderTop : '#333236',
-    postBorderTop : '#333236',
-    postOption : '#a29b9f',
-    text : '#a29b9f',
-    textField : '#444148',
-    posts : '#2a292e',
-    header : '#2a292e',
-    chatToolbar : '#393b40'
-}
-    
+
+const darkModeColours = [
+    {hexColourCode : '#18191a', cssVariable: "--fb-theme-colour-background"},
+    {hexColourCode : '#a29b9f', cssVariable: "--fb-theme-colour-storyreel"},
+    {hexColourCode : '#a29b9f', cssVariable: "--fb-theme-colour-arrow"},
+    {hexColourCode : '#a29b9f', cssVariable: "--fb-theme-colour-post-text"},
+    {hexColourCode : '#333236', cssVariable: "--fb-theme-colour-messenger-sender-border-top"},
+    {hexColourCode : '#333236', cssVariable: "--fb-theme-colour-post-border-top"},
+    {hexColourCode : '#a29b9f', cssVariable: "--fb-theme-colour-post-option"},
+    {hexColourCode : '#a29b9f', cssVariable: "--fb-theme-colour-text"},
+    {hexColourCode : '#444148', cssVariable: "--fb-theme-colour-textfield"},
+    {hexColourCode : '#2a292e', cssVariable: "--fb-theme-colour-white-posts"},
+    {hexColourCode : '#2a292e', cssVariable: "--fb-theme-colour-white-header"},
+    {hexColourCode : '#393b40', cssVariable: "--fb-theme-colour-chat-toolbar"}
+]
+        
 export default setPropertyOfDarkMode;
--- src/functions/LightMode.js
+++ src/functions/LightMode.js
@@ -1,31 +1,20 @@
 const setPropertyOfLightMode = () => {
-    document.documentElement.style.setProperty("--fb-theme-colour-white-header", lightModeColours.header);
-    document.documentElement.style.setProperty("--fb-theme-colour-white-posts", lightModeColours.posts);
-    document.documentElement.style.setProperty("--fb-theme-colour-textfield", lightModeColours.textField);
-    document.documentElement.style.setProperty("--fb-theme-colour-text", lightModeColours.text);
-    document.documentElement.style.setProperty("--fb-theme-colour-post-option", lightModeColours.postOption);
-    document.documentElement.style.setProperty("--fb-theme-colour-post-border-top", lightModeColours.postBorderTop);
-    document.documentElement.style.setProperty("--fb-theme-colour-messenger-sender-border-top", lightModeColours.messageBorderTop);
-    document.documentElement.style.setProperty("--fb-theme-colour-post-text", lightModeColours.postText);
-    document.documentElement.style.setProperty("--fb-theme-colour-arrow", lightModeColours.arrow);
-    document.documentElement.style.setProperty("--fb-theme-colour-storyreel", lightModeColours.storyReel);
-    document.documentElement.style.setProperty("--fb-theme-colour-background", lightModeColours.background);
-    document.documentElement.style.setProperty("--fb-theme-colour-chat-toolbar", lightModeColours.chatToolbar);
+    lightModeColours.map((lightColours) => document.documentElement.style.setProperty(lightColours.cssVariable, lightColours.hexColourCode));
 }
     
-const lightModeColours = {
-    background : '#f1f2f5',
-    storyReel : '#2e81f4',
-    arrow : 'rgba(0, 0, 0, 0.54)',
-    postText : '#000000',
-    messageBorderTop : '#eff2f5',
-    postBorderTop : 'lightgray',
-    postOption :'gray',
-    text : '#000000',
-    textField : '#eff2f5',
-    posts : 'white',
-    header : 'white',
-    chatToolbar : 'royalblue'
-}
-    
+const lightModeColours = [
+    {hexColourCode : '#f1f2f5', cssVariable: "--fb-theme-colour-background"},
+    {hexColourCode : '#2e81f4', cssVariable: "--fb-theme-colour-storyreel"},
+    {hexColourCode : 'rgba(0, 0, 0, 0.54)', cssVariable: "--fb-theme-colour-arrow"},
+    {hexColourCode : '#000000', cssVariable: "--fb-theme-colour-post-text"},
+    {hexColourCode : '#eff2f5', cssVariable: "--fb-theme-colour-messenger-sender-border-top"},
+    {hexColourCode : 'lightgray', cssVariable: "--fb-theme-colour-post-border-top"},
+    {hexColourCode :'gray', cssVariable: "--fb-theme-colour-post-option"},
+    {hexColourCode : '#000000', cssVariable: "--fb-theme-colour-text"},
+    {hexColourCode : '#eff2f5', cssVariable: "--fb-theme-colour-textfield"},
+    {hexColourCode : 'white', cssVariable: "--fb-theme-colour-white-posts"},
+    {hexColourCode : 'white', cssVariable: "--fb-theme-colour-white-header"},
+    {hexColourCode : 'royalblue', cssVariable: "--fb-theme-colour-chat-toolbar"}
+]
+
 export default setPropertyOfLightMode;
