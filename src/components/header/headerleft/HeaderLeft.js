98bce31bd4341c6d1c8dd3626f09a4b79cad89c2
SI-Encoding <stanscoding12+encoding@gmail.com>
2023-02-01 18:40:46 -0800

    Modified header left component.

--- src/components/header/headerleft/HeaderLeft.js
+++ src/components/header/headerleft/HeaderLeft.js
@@ -6,8 +6,8 @@ export default function HeaderLeft() {
     <div className = "header_left">
         <img src="https://1000logos.net/wp-content/uploads/2016/11/Facebook-logo-500x350.png" alt="fb_logo"/>
         <div className="header__input">
-            <SearchIcon style={{color:'var(--fb-theme-colour-arrow)'}}/>
-            <input placeholder="Search Facebook" type="text" disabled/>
+            {/* <SearchIcon style={{color:'var(--fb-theme-colour-arrow)'}}/> */}
+            <input placeholder="Welcome to Fb Clone" type="text" disabled/>
         </div>
     </div>
   )
